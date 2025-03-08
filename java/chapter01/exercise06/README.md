fa46313077d6702e082edf893ec920f91c6ba0cc
Damien Pichard <damienpichard@tuta.com>
2025-03-08 02:31:01 +0100

    [JAVA] — Chapter 01 — Exercise 06

---
+++ java/chapter01/exercise06/README.md
@@ -0,0 +1,17 @@
+<div align="center">
+
+# Chapter 01 — Exercice 06
+
+[previous](../exercise05) • [next](../../chapter02/exercise01)
+
+![Latest commit](https://img.shields.io/github/last-commit/damienpichard/dotfiles/main?style=for-the-badge)
+[![GPL-3.0 License](https://img.shields.io/badge/LICENSE-GPL--v3-blue?logo=GNU&style=for-the-badge)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+</div>
+
+
+
+> Modify `UseArgument.java` to make a program `UseThree.java` that takes
+> three names as command-line arguments and prints a proper sentence with
+> the names in the reverse of the order given, so that, for example,
+> `java UseThree Alice Bob Carol` prints `Hi Carol, Bob, and Alice.`
---
+++ java/chapter01/exercise06/main.java
@@ -0,0 +1,28 @@
+// Copyright © Damien Pichard 2025
+// Author: Damien Pichard <damienpichard@tuta.com>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Code:
+
+
+public class main { 
+  public static void main(String[] args) {
+    System.out.print("Hi ");
+    System.out.print(args[2]);
+    System.out.print(", ");
+    System.out.print(args[1]);
+    System.out.print(", and ");
+    System.out.print(args[0]);
+    System.out.println(".");
+  } 
+}
