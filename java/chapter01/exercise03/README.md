c58a9a2f294b6a927aab5d8be741bc56bbb8366a
Damien Pichard <damienpichard@tuta.com>
2025-03-08 02:30:19 +0100

    [JAVA] — Chapter 01 — Exercise 03

---
+++ java/chapter01/exercise03/README.md
@@ -0,0 +1,22 @@
+<div align="center">
+
+# Chapter 01 — Exercice 03
+
+[previous](../exercise02) • [next](../exercise04)
+
+![Latest commit](https://img.shields.io/github/last-commit/damienpichard/dotfiles/main?style=for-the-badge)
+[![GPL-3.0 License](https://img.shields.io/badge/LICENSE-GPL--v3-blue?logo=GNU&style=for-the-badge)](https://www.gnu.org/licenses/gpl-3.0.en.html)
+
+</div>
+
+
+
+> Describe what happens if you misspell (by, say, omitting the second letter) the following in `HelloWorld.java`:
+>
+>   a. public
+>
+>   b. static
+>
+>   c. void
+>
+>   d. args
---
+++ java/chapter01/exercise03/mainc.java
@@ -0,0 +1,24 @@
+// Copyright © Damien Pichard 2025
+// Author: Damien Pichard <damienpichard@tuta.com>
+
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+// Code:
+
+
+public class mainc { 
+  public static viod main(String[] args) {
+    // Prints "Hello, World" in the terminal window.
+    System.out.println("Hello, World"); 
+  } 
+}
+
